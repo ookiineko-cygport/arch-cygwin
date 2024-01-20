6af5b4f16910344528ca5f10ad8bcaf3a62505aa
xantares <>
2024-01-20 14:17:45 +0800

    cygwin-configure: add package
    Co-authored-by: Michel Zou <>
    
    Signed-off-by: Ookiineko <chiisaineko@protonmail.com>

---
+++ cygwin-configure/cygwin-configure.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+. /usr/bin/cygwin-env @TRIPLE@
+
+# check if last arg is a path to configure, else use parent
+for last; do true; done
+if test -x "${last}/configure"; then
+  config_path="$last"
+else
+  config_path=".."
+fi
+
+${config_path}/configure \
+  --host=@TRIPLE@ --target=@TRIPLE@ --build="$CHOST" \
+  --prefix=/usr/@TRIPLE@ --libdir=/usr/@TRIPLE@/lib --includedir=/usr/@TRIPLE@/include \
+  --enable-shared --enable-static "$@"
